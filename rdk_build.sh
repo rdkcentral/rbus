8e4939038665b57a5077a9faf20e3bb398322147
Karunakaran A <48997923+karuna2git@users.noreply.github.com>
2022-11-23 17:58:28 -0500

    Enable RDKLogger and Disable Breakpadwrap Support (#48)
    * Fixed the FSROOT PATH for RDKC & Disable the breakpad support for RDKC for now
    
    Signed-off-by: Karunakaran A <karunakaran_amirthalingam@cable.comcast.com>

--- rdk_build.sh
+++ rdk_build.sh
@@ -56,8 +56,8 @@ if [ "$XCAM_MODEL" == "SCHC2" ]; then
     EXTRA_OPTIONS+="-DWITH_SPAKE2=ON"
     if [ "$RDK_COMPONENT_NAME" == "xwrbus" ]; then
         echo "Setting environmental variables and Pre rule makefile for xw-xCam2"
+        export RDK_XW_FSROOT_PATH=${RDK_XW_FSROOT_PATH-`readlink -m $RDK_PROJECT_ROOT_PATH/xw/sdk/fsroot/ramdisk`}
         source ${RDK_PROJECT_ROOT_PATH}/build/components/realtek/sdk/setenv2
-        export RDK_XW_FSROOT_PATH=${RDK_FSROOT_PATH}
         export INSTALL_PATH="$RDK_XW_FSROOT_PATH/usr"
     else
         echo "Setting environmental variables and Pre rule makefile for xCam2"
@@ -76,7 +76,7 @@ fi
 
 #export COMP_BASE_PATH=${RDK_SCRIPTS_PATH%/*}
 export COMP_BASE_PATH=${RDK_SCRIPTS_PATH}/../
-export SEARCH_PATH="$RDK_TARGET_PATH"
+export SEARCH_PATH="$RDK_FSROOT_PATH/usr;$RDK_FSROOT_PATH/usr/local"
 
 #The cross compile tools are exported already in XHB1;lets avoid only that (Because SOURCETOOLCHAIN is not exported in XHB1)
 if [ "$XCAM_MODEL" != "XHB1" ]; then
@@ -127,8 +127,6 @@ ARGS=$@
 
 if [ "$RDK_COMPONENT_NAME" == "xwrbus" ]; then
    EXTRA_OPTIONS+=" -DBUILD_RBUS_DAEMON=OFF -DBUILD_RBUS_SAMPLE_APPS=OFF -DBUILD_RBUS_TEST_APPS=OFF -DBUILD_ONLY_RTMESSAGE=ON"
-else
-   EXTRA_OPTIONS+=" -DINCLUDE_BREAKPAD=ON"
 fi
 
 # functional modules
