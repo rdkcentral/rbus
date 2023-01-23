a1d8f8d60a06a3a7124957dc967241797bbf216a
gururaajar <83449026+gururaajar@users.noreply.github.com>
2023-01-23 16:48:50 -0500

    Fix the buffer size to 8K for Camera devices (#92)
    Co-authored-by: Karunakaran A <48997923+karuna2git@users.noreply.github.com>
    Co-authored-by: Karunakaran A <Karunakaran_Amirthalingam@cable.comcast.com>

--- src/rtmessage/rtConnection.c
+++ src/rtmessage/rtConnection.c
@@ -61,7 +61,11 @@ typedef volatile int atomic_uint_least32_t;
 #include <sys/time.h>
 
 #define RTMSG_LISTENERS_MAX 64
+#ifdef  RDKC_BUILD
+#define RTMSG_SEND_BUFFER_SIZE (1024 * 8)
+#else
 #define RTMSG_SEND_BUFFER_SIZE (1024 * 64)
+#endif /* RDKC_BUILD */
 #ifndef SOL_TCP
 #define SOL_TCP 6
 #endif
