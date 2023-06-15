066ffccafbf97b9f356e26d98735a838bbeaf63d
Karunakaran A <48997923+karuna2git@users.noreply.github.com>
2023-06-15 15:59:57 -0400

    Update rbusOpenTelemetry.c

--- sampleapps/consumer/rbusOpenTelemetry.c
+++ sampleapps/consumer/rbusOpenTelemetry.c
@@ -40,21 +40,17 @@ int main(int argc, char *argv[])
 
   if (argc > 1)
   {
-      if(argc == 3)
-      {
-          runtime = atoi(argv[2]);
-      }
       if (strcmp(argv[1], "manual") == 0)
       {
           run_manual_client();
       }
       else
       {
-          run_server();
+          run_client();
       }
   }
   else
-      run_client();
+      run_server();
   return 0;
 }
 
