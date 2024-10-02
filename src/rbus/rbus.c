f4a63d59934d76c8a0f52e162365633f5d45d345
pradeeptakdas <37864641+pradeeptakdas@users.noreply.github.com>
2024-10-02 15:31:59 -0400

    Update rbus.c

--- src/rbus/rbus.c
+++ src/rbus/rbus.c
@@ -2715,7 +2715,7 @@ static int _callback_handler(char const* destination, char const* method, rbusMe
         /* For the components register directly with rbus_open,
          * return success for getAttributes with 0 parameters.*/
         rbusMessage_Init(response);
-        rbusMessage_SetInt32(*response, RBUS_ERROR_SUCCESS);
+        rbusMessage_SetInt32(*response, RBUS_ERROR_INVALID_METHOD);
     }
     else if(!strcmp(method, METHOD_SUBSCRIBE) || !strcmp(method, METHOD_UNSUBSCRIBE))
     {
