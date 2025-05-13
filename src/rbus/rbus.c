172b46cc85f3dd01b5ede78435573d8a2bfff73b
Netaji Panigrahi <114923459+NetajiPanigrahi@users.noreply.github.com>
2025-05-13 20:25:37 +0530

    RDKB-59722 : memory leak in rbus_getExt (#258)
    Reason for change: Identified a memory leak in rbus_getExt() when it called with single linear parameter.
    Test Procedure: run with valgrind
    Risks: low
    ---------
    
    Co-authored-by: Karunakaran A <48997923+karuna2git@users.noreply.github.com>

--- src/rbus/rbus.c
+++ src/rbus/rbus.c
@@ -3616,7 +3616,13 @@ rbusError_t rbus_getExt(rbusHandle_t handle, int paramCount, char const** pParam
         if (!_is_wildcard_query(pParamNames[0]))
         {
             rbusProperty_t outputVals = NULL;
-            rbusValue_t getVal;
+            rbusValue_t getVal = NULL;
+
+            /* Initialize */
+            *retProperties = NULL;
+            *numValues = 0;
+
+            /*Do get the single param */
             errorcode = rbus_get(handle, pParamNames[0], &getVal);
             if(RBUS_ERROR_SUCCESS == errorcode)
             {
@@ -3625,6 +3631,11 @@ rbusError_t rbus_getExt(rbusHandle_t handle, int paramCount, char const** pParam
                 rbusValue_Release(getVal);
                 *retProperties = outputVals;
             }
+	    else
+	    {
+		RBUSLOG_ERROR("Failed to get the data. Error : %d", errorcode);
+	    }
+	    return errorcode;	
         }
         else
         {
@@ -3728,7 +3739,6 @@ rbusError_t rbus_getExt(rbusHandle_t handle, int paramCount, char const** pParam
                     {
                         return errorcode;
                     }
-
                 }
             }
             else
