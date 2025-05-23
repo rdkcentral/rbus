f0b3ad1c1e9c8df631266e3ede6d5cba04aaae8d
rosemarybennyy <RoseMary_Benny@comcast.com>
2025-05-23 17:10:18 +0530

    Update rbusTestProvider.c

--- test/rbus/provider/rbusTestProvider.c
+++ test/rbus/provider/rbusTestProvider.c
@@ -1424,8 +1424,8 @@ int main(int argc, char *argv[])
     int i, j;
     int loopFor = 0;
     rtLogLevel logLvl = RT_LOG_WARN;
-    int *data = malloc(sizeof(int));
-	*data = 5;
+    int *rose = malloc(sizeof(int));
+	*rose = 5;
 	
 
     printf("provider: start\n");
