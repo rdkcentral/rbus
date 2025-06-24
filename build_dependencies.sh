2bfef89c1c69330c5cdd50fbebe4c6c089118cf7
Karunakaran A <karunakaran_amirthalingam@cable.comcast.com>
2025-06-24 13:55:07 -0400

    Added Required scripts to run coverity
    Signed-off-by: Karunakaran A <karunakaran_amirthalingam@cable.comcast.com>

---
+++ build_dependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+set -x
+
+##############################
+#      Setup WorkSpace       #
+##############################
+GITHUB_WORKSPACE="${PWD}"
+ls -la ${GITHUB_WORKSPACE}
+cd ${GITHUB_WORKSPACE}
+
+#######################################
+#  Install Dependencies and packages  #
+#######################################
+apt update
+apt update && sudo apt install -y libcurl4-openssl-dev libgtest-dev lcov gcovr libmsgpack* libcjson-dev  build-essential
+
