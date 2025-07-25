c921b482f386be142f589547e141da35a853f1b5
Karunakaran A <48997923+karuna2git@users.noreply.github.com>
2025-07-24 22:30:24 -0400

    Update rtString.c

--- src/rtmessage/rtString.c
+++ src/rtmessage/rtString.c
@@ -1,19 +1,18 @@
 #include "rtString.h"
-#include <stdio.h>
-#include <string.h>
+#include "rtLog.h"
 
 uint32_t rtString_Copy(char* dstBuffer, const char* srcBuffer, uint32_t dstBufferSize)
 {
-   if ((0 == dstBufferSize) || (!srcBuffer) || (!dstBuffer))
-        return 0;
+  uint32_t srcBufferSize = 0;
+  if ((0 == dstBufferSize) || (!srcBuffer) || (!dstBuffer))
+    return 0;
 
-    uint32_t srcBufferSize = snprintf(dstBuffer, dstBufferSize, "%s", srcBuffer);
-
-    if (srcBufferSize >= dstBufferSize)
-        printf("Output was truncated.\n");
-
-    if (dstBufferSize <= srcBufferSize)
-        return dstBufferSize;
-    else
-        return srcBufferSize;
+  srcBufferSize = snprintf(dstBuffer, dstBufferSize, "%s", srcBuffer);
+  if (srcBufferSize > dstBufferSize)
+  {
+     rtLog_Info("Truncated the given string (%s) as (%s) with size %d", srcBuffer, dstBuffer, dstBufferSize);
+     return dstBufferSize;
+  }
+  else
+    return srcBufferSize;
 }
