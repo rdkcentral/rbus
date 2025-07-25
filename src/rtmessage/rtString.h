e2c84c4fb9d509a9b3510ba0b23985bdf9426965
Karunakaran A <48997923+karuna2git@users.noreply.github.com>
2025-07-24 22:24:07 -0400

    Update rtString.h

--- src/rtmessage/rtString.h
+++ src/rtmessage/rtString.h
@@ -7,7 +7,7 @@
 extern "C" {
 #endif
 
-// Safe string copy: copies up to (size-1) characters and null-terminates
+// Copies up to (size - 1) characters and null-terminates
 uint32_t rtString_Copy(char* dstBuffer, const char* srcBuffer, uint32_t dstBufferSize);
 
 #ifdef __cplusplus
