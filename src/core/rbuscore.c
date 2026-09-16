3968683701ffc3255e7f8276abe5f59f53aa0e43
Padala Hanuk Kumar <hanuk.padala@gmail.com>
2026-09-16 20:07:09 +0530

    Update rbuscore.c

--- src/core/rbuscore.c
+++ src/core/rbuscore.c
@@ -466,8 +466,10 @@ static server_object_t get_object(const char * object_name)
 
 static rbusCoreError_t translate_rt_error(rtError err)
 {
-    if(RT_OK == err)
+    if(RT_OK == err){
+     RBUSCORELOG_DEBUG("translate_rt_error: success to RBUSCORE_ERROR_GENERAL");
         return RBUSCORE_SUCCESS;
+    }
     else
        {
         RBUSCORELOG_DEBUG("translate_rt_error: mapping rtError %d to RBUSCORE_ERROR_GENERAL", err);
